a5da290f7ad776e367975d77646d38b7838ed3ba
bluezebragames <bluezebragames@gmail.com>
2015-06-07 13:44:57 -0600

    Some "meh" changes

--- js/main.js
+++ js/main.js
@@ -1,37 +1,43 @@
+/*
+    Helper functions!
+    I never really understood the distinction between helper functions and regular
+    functions...  All functions are supposed to be helpful (right?)
+*/
+
 // beautify a number
 var fix = function(num)
 {
-    // first of all, is it > 1,000,000?
+    // first of all, is it >= 1,000,000?
 
-    if(num > 1000000)
+    if(num >= 1000000)
     {
-        var numdigits = Math.floor(Math.log(num) / Math.log(10));
-        var numgroupsofthree = numdigits / 3;
-        numdigits -= numgroupsofthree;
+        var numdigits = Math.floor(Math.log(num) / Math.log(10) + .0001);
+        var numgroupsofthree = Math.floor(numdigits / 3);
+        numdigits -= numdigits % 3;
         switch (numgroupsofthree)
         {
         case 2:
-            var postfix = "Million";
+            var postfix = "million";
             break;
         case 3:
-            var postfix = "Billion";
+            var postfix = "billion";
             break;
         case 4:
-            var postfix = "Trillion";
+            var postfix = "trillion";
             break;
         case 5:
-            var postfix = "Quadrillion";
+            var postfix = "quadrillion";
             break;
         case 6:
-            var postfix = "Quintillion";
+            var postfix = "quintillion";
             break;
         default:
             var postfix = "...  A lot";
         }
-        return (num / Math.pow(10, numdigits)) + " " + postfix;
+        return Math.floor((1000 * num / Math.pow(10, numdigits)) + .001) / 1000 + " " + postfix;
     }
 
-    return Math.round(num);
+    return Math.floor(num + .001);
 }
 
 
@@ -64,6 +70,10 @@ Game.tooltip.hide = function(){
 
 }*/
 
+/*
+    SETUP STUPH!  But then again it's almost all setup stuph
+*/
+
 // resets things and loads the save from localstorage
 Game.Launch = function() {
     Game.wps = 0;
@@ -73,8 +83,10 @@ Game.Launch = function() {
     Game.priceScale = 1.15;
     Game.fps = 60;
     Game.buildings = [];
-    Game.buildings.push(new Building(10,0.1));
-    Game.buildings.push(new Building(10000,500));
+    Game.buildings.push(new Building(15,0.1,0,"B1"));
+    Game.buildings.push(new Building(100,1,0,"B2"));
+    Game.buildings.push(new Building(10000,500,0,"B3"));
+    Game.buildBuildings();
 
     //Game.tooltip(Game.buildings[0].tooltip());
     if (localStorage.getItem("game"))
@@ -87,10 +99,39 @@ Game.Launch = function() {
     
 }
 
+// creates divs and buttons for buildings
+Game.buildBuildings = function() {
+    for(b in Game.buildings)
+    {
+        // create stat div on left panel
+        var div = document.createElement("div");
+        div.id = b + "stats";
+        var leftpanel = document.getElementById("leftpanel");
+        leftpanel.appendChild(div);
+
+        // create buttons on right panel
+        div = document.createElement("div");
+        var b1 = b + 1;
+        div.innerHTML = '<button id="button'+b+'">Buy one <span id="bname'+b+'">"Building '+b1+'"</span></button><span id="b'+b+'"></span>'
+        div.id = b;
+        var rightpanel = document.getElementById("rightpanel");
+        rightpanel.appendChild(div);
+    }
+}
+
+/*
+    SAVING (and for some reason, renaming your character...  whoops)
+*/
+
 // import the save file as text
 Game.importSave = function() {
     var sve = prompt("Paste save here!");
     Game.loadSave(sve);
+}
+
+// export the save file as text
+Game.exportSave = function() {
+    prompt("Copy and paste this save!", Game.Save());
 }
 
 // the onclick function for the name
@@ -115,6 +156,8 @@ Game.Save = function() {
     var middle = document.getElementById("middlepanel");
     middle.innerHTML = "Game Saved";
 
+    return str;
+
     setTimeout(Game.Save, 10000);
     setTimeout(Game.clearMiddle, 1500);
 }
@@ -132,14 +175,22 @@ Game.loadSave = function(file) {
     Game.recalculate();
 }
 
+/*
+    PRESTIGE!
+*/
+
 // reset the game!
 Game.reset = function(kind)
 {
     if(kind)
     {
-        Game.loadSave("0|0,0,||");
+        Game.loadSave("0|0,0,0,||");
     }
 }
+
+/*
+    GAME LOGIC!
+*/
 
 // add things when you click!
 Game.click = function() {
@@ -154,16 +205,16 @@ Game.recalculate = function(){
 }
 
 // have any buildings become affordable?
-// have any buildings become expensive?
+// have any buildings become too expensive?
 Game.checkBuildings = function(){
     for(b in Game.buildings)
     {
-        if(Game.words <= Game.buildings[b].price)
+        if(Game.words < Game.buildings[b].price)
         {
             var temp = document.getElementById("button"+b);
             temp.className = "grayed";
         }
-        else if(Game.words > Game.buildings[b].price)
+        else if(Game.words >= Game.buildings[b].price)
         {
             var temp = document.getElementById("button"+b);
             temp.className = "notgrayed";
@@ -201,9 +252,13 @@ Game.buyBuildings = function(whichBuilding) {
 // get words from idling
 Game.Logic = function() {
     Game.words += Game.wps/Game.fps;
-    Game.wordsd = Math.round(Game.words);
+    Game.wordsd = fix(Game.words);
     Game.recalculate();
 }
+
+/*
+    DRAWING!
+*/
 
 // draw everything!  mostly a wrapper function
 Game.Draw = function() {
@@ -228,10 +283,26 @@ Game.drawPic = function() {
 Game.drawBuildings = function() {
     for(b in Game.buildings)
     {
+        // inside the button
+        var spn = document.getElementById("bname" + b);
+        spn.innerHTML = Game.buildings[b].called;
+
+        // outside the button
         var spn = document.getElementById("b" + b);
         spn.innerHTML = " Price: " + fix(Game.buildings[b].price) + " words";
     }
 }
+
+// clear the middle because it was overwritten by "game saved"
+Game.clearMiddle = function() {
+    var middle = document.getElementById("middlepanel");
+    middle.innerHTML = "Middle";
+    //alert("ao8ueroi");
+}
+
+/*
+    Let's do this thing!
+*/
 
 // everything that happens every frame
 Game.Loop = function () {
@@ -242,8 +313,7 @@ Game.Loop = function () {
     for(b in Game.buildings)
     {
         var currDiv = document.getElementById(b+"stats");
-        var temp = Math.round(b)+1;
-        currDiv.innerHTML = "You have " + Game.buildings[b].howMany + " of building " + temp;
+        currDiv.innerHTML = "You have " + Game.buildings[b].howMany + " " + Game.buildings[b].called + (Game.buildings[b].howMany == 1 ? "" : "s");
     }
 
     Game.catchuplogic = 0;
@@ -251,12 +321,6 @@ Game.Loop = function () {
     Game.Draw();
 
     setTimeout(Game.Loop, 1000/Game.fps);
-}
-
-// clear the middle because it was overwritten by "game saved"
-Game.clearMiddle = function() {
-    var middle = document.getElementById("middlepanel");
-    middle.innerHTML = "Middle";
 }
 
 window.onload = function() {
